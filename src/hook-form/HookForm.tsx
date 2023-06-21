1e82b40086e2b2ce0e6866f6866a0370a05845cb
Sunny <heysunny612@gmail.com>
2023-06-21 18:39:42 +0900

    React-Hook-Form: Form Errors 핸들링 방법

--- src/hook-form/HookForm.tsx
+++ src/hook-form/HookForm.tsx
@@ -1,8 +1,25 @@
 import { useForm } from 'react-hook-form';
+
+interface IForm {
+  email: string;
+  first_name: string;
+  last_name: string;
+  password: string;
+  password_check: string;
+}
+
 export default function HookForm() {
-  const { register, handleSubmit, formState } = useForm();
-  const onVaild = (data: any) => {};
-  console.log(formState.errors);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: '@naver.com',
+    },
+  });
+  const onVaild = (data) => {};
+
   return (
     <form
       onSubmit={handleSubmit(onVaild)}
@@ -10,9 +27,16 @@ export default function HookForm() {
     >
       <input
         type='text'
-        {...register('email', { required: '이메일을 입력해주세요' })}
+        {...register('email', {
+          required: '이메일을 입력해주세요',
+          pattern: {
+            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+            message: '네이버의 이메일만 입력 가능합니다.',
+          },
+        })}
         placeholder='Email'
       />
+      {errors.email && <span>{errors?.email?.message + ''}</span>}
       <input
         type='text'
         {...register('first_name', {
@@ -21,21 +45,29 @@ export default function HookForm() {
         })}
         placeholder='First Name'
       />
+      {errors.first_name && <span>{errors?.first_name?.message + ''}</span>}
       <input
         type='text'
-        {...register('last_name', { required: true })}
+        {...register('last_name', { required: '성을 입력해주세요' })}
         placeholder='Last Name'
       />
+      {errors.last_name && <span>{errors?.last_name?.message + ''}</span>}
       <input
         type='text'
-        {...register('password', { required: true })}
+        {...register('password', { required: '비밀번호를 입력해주세요' })}
         placeholder='Password'
       />
+      {errors.password && <span>{errors?.password?.message + ''}</span>}
       <input
         type='text'
-        {...register('password_check')}
+        {...register('password_check', {
+          required: '두개의 비밀번호가 맞지않습니다',
+        })}
         placeholder='Password Check'
       />
+      {errors.password_check && (
+        <span>{errors?.password_check?.message + ''}</span>
+      )}
       <button>Add</button>
     </form>
   );
