e1ba1c8e8c3edac00e165ff9b68a6fe7860faa1c
Sunny <heysunny612@gmail.com>
2023-06-21 18:10:38 +0900

    React-Hook-Form: formState.errors를 이용한 errors 핸들링 방법

--- src/hook-form/HookForm.tsx
+++ src/hook-form/HookForm.tsx
@@ -1,14 +1,36 @@
 import { useForm } from 'react-hook-form';
 export default function HookForm() {
-  const { register, watch } = useForm();
-  console.log(watch());
-
+  const { register, handleSubmit, formState } = useForm();
+  const onVaild = (data: any) => {};
+  console.log(formState.errors);
   return (
-    <form>
-      <input type='text' {...register('email')} placeholder='Email' />
-      <input type='text' {...register('first_name')} placeholder='First Name' />
-      <input type='text' {...register('last_name')} placeholder='Last Name' />
-      <input type='text' {...register('password')} placeholder='Password' />
+    <form
+      onSubmit={handleSubmit(onVaild)}
+      style={{ display: 'flex', flexDirection: 'column' }}
+    >
+      <input
+        type='text'
+        {...register('email', { required: '이메일을 입력해주세요' })}
+        placeholder='Email'
+      />
+      <input
+        type='text'
+        {...register('first_name', {
+          required: '이름을 입력해주세요',
+          minLength: { value: 5, message: 'Your password is too short' },
+        })}
+        placeholder='First Name'
+      />
+      <input
+        type='text'
+        {...register('last_name', { required: true })}
+        placeholder='Last Name'
+      />
+      <input
+        type='text'
+        {...register('password', { required: true })}
+        placeholder='Password'
+      />
       <input
         type='text'
         {...register('password_check')}
